fe72592c6767bf514df19426ef843c51118ff6a6
trekfan42 <davisam42@gmail.com>
2023-01-16 17:14:10 -0500

    V2-Test
    Checking Icon in PWA

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1673838165|22641630203";
+const CACHE_VERSION = "1673907148|97284615";
 const CACHE_PREFIX = "MiniGolf Scoreca-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";

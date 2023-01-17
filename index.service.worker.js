9874c98b2e9df53310201b89dec114b4d0eb6c61
trekfan42 <davisam42@gmail.com>
2023-01-17 08:46:54 -0500

    V14
    Test anim texture rect

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1673962805|797377030";
+const CACHE_VERSION = "1673963175|1166888958";
 const CACHE_PREFIX = "MiniGolf Scoreca-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
